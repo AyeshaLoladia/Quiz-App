5d31d93097287803c859030b0b697a2f66f48012
Ayesha Loladia <ayeshaloladia13@gmail.com>
2024-01-04 13:04:12 +0530

    start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(body: ,),
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 128, 93, 188),
+            Color.fromARGB(255, 43, 23, 78)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: const StartScreen(),
+      ),
+    ),
   ));
 }
